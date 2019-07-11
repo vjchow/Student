57f90aff4a0f5c563b4c32a75dddcec9b19c8804
csund <csund@VIRUS>
2019-07-11 00:01:20 -0400

    Commit to Master

--- restws-1/src/main/java/com/vijay/restws/entities/EmployeeEntities.java
+++ restws-1/src/main/java/com/vijay/restws/entities/EmployeeEntities.java
@@ -1,12 +1,10 @@
 package com.vijay.restws.entities;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties
-@PropertySource("classpath:employee.properties")
 public class EmployeeEntities {
 
 	private int id;
--- restws-1/src/main/java/com/vijay/restws/impl/EmployeeImpl.java
+++ restws-1/src/main/java/com/vijay/restws/impl/EmployeeImpl.java
@@ -1,23 +1,35 @@
 package com.vijay.restws.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.vijay.restws.entities.EmployeeEntities;
 
-@Component
+@Configuration
+@PropertySource("classpath:employee.properties")
 public class EmployeeImpl {
-	
-	@Autowired
-	private EmployeeEntities employeeEntities;
 
-	@Scheduled(cron="0/1 * * * * ?")
+	@Value("${employee.id}")
+	private String employeeId;
+
+	@Value("${employee.name}")
+	private String employeeName;
+
+	@Value("${employee.address}")
+	private String employeeAddress;
+
+	@Scheduled(cron = "0/1 * * * * ?")
 	public void print() {
-					
-		System.out.println(employeeEntities.getId() + employeeEntities.getName() + employeeEntities.getAddress());	
-		System.out.println("This clsas is not being executed......");
+
+		System.out.println("The Employee Id is..." + employeeId);
+		System.out.println("The Employee Name is..." + employeeName);
+		System.out.println("The Employee Address is..." + employeeAddress);
+
+		System.out.println("This class is not being executed......");
 	}
-	
 
 }
